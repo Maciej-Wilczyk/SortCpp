63f66a0171217baccaf818b6ae7fd69a44225856
Maciej-Wilczyk <61824722+Maciej-Wilczyk@users.noreply.github.com>
2023-09-15 19:53:17 +0200

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,24 +1,76 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
-#include <chrono>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <sys/sysinfo.h>
 
-int main() {
-    size_t array_size = 500000000;  
-    std::vector<double> large_array(array_size);
+void sortAndSave(std::vector<int>& numbers, int runIndex) {
+    std::sort(numbers.begin(), numbers.end());
+    std::ofstream output("run" + std::to_string(runIndex), std::ios::binary);
+    for (int num : numbers) {
+        output.write(reinterpret_cast<char*>(&num), sizeof(int));
+    }
+}
 
-    // Wypełnianie tablicy losowymi wartościami
-    for(size_t i = 0; i < array_size; ++i) {
-        large_array[i] = static_cast<double>(rand()) / RAND_MAX;  // Losowe wartości typu double w zakresie [0, 1)
+void externalSort(const std::string &inputFile, const std::string &outputFile, size_t memorySize) {
+    std::ifstream input(inputFile, std::ios::binary);
+    std::vector<int> buffer(memorySize);
+    int runIndex = 0;
+
+    while (input.read(reinterpret_cast<char*>(buffer.data()), buffer.size() * sizeof(int))) {
+        sortAndSave(buffer, runIndex);
+        ++runIndex;
     }
 
-    // Pomiar czasu sortowania
-    // auto start = std::chrono::high_resolution_clock::now();
-    std::sort(large_array.begin(), large_array.end());
-    // auto stop = std::chrono::high_resolution_clock::now();
-    // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
+    std::vector<std::ifstream> runs(runIndex);
 
-    std::cout << "Sorting end";
+    for (int i = 0; i < runIndex; ++i) {
+        runs[i].open("run" + std::to_string(i), std::ios::binary);
+        int num;
+        runs[i].read(reinterpret_cast<char*>(&num), sizeof(int));
+        pq.push({ num, i });
+    }
+
+    std::ofstream output(outputFile, std::ios::binary);
+    while (!pq.empty()) {
+        auto [num, i] = pq.top();
+        pq.pop();
+
+        output.write(reinterpret_cast<char*>(&num), sizeof(int));
+
+        if (runs[i].read(reinterpret_cast<char*>(&num), sizeof(int))) {
+            pq.push({ num, i });
+        }
+    }
+}
+
+int main() {
+    // Create input binary file
+    std::ofstream input("input.bin", std::ios::binary);
+    for (int i = 100; i >= 0; --i) {
+        input.write(reinterpret_cast<char*>(&i), sizeof(int));
+    }
+
+    // External sort
+    externalSort("input.bin", "output.bin", 10);
+
+    // Read output binary file
+    std::ifstream output("output.bin", std::ios::binary);
+    struct sysinfo info;
+    if (sysinfo(&info) == 0) {
+        size_t memorySize = info.freeram / 4; // Ustal rozmiar buforu jako ćwiartkę dostępnej pamięci RAM
+        std::cout << "Using buffer size: " << memorySize << " bytes." << std::endl;
+        externalSort("input.bin", "output.bin", memorySize);
+    }
+    // int num;
+    // while (output.read(reinterpret_cast<char*>(&num), sizeof(int))) {
+    //     std::cout << num << " ";
+    // }
+    // std::cout << std::endl;
 
     return 0;
 }
